523fc3f0fa890d82fede6ace6153db34f82f4cac
Mathze <270275+mathze@users.noreply.github.com>
2023-01-21 15:56:11 +0100

    Update all dependencies
    - Updated dependencies and build script to new kotlin requirements
    - Added java-toolchain
    - Fixed workflow
    
    Signed-off-by: Mathze <270275+mathze@users.noreply.github.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 pluginManagement {
-  val kotlinVersion = "1.7.22"
+  val kotlinVersion = "1.8.0"
   repositories {
     gradlePluginPortal()
   }
@@ -9,7 +9,7 @@ pluginManagement {
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
 
-    id("org.springframework.boot") version "3.0.0"
+    id("org.springframework.boot") version "3.0.2"
     id("io.spring.dependency-management") version "1.1.0"
 
     // quality
